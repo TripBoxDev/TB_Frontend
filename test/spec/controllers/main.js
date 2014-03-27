72518924fcecba81a8486231ac155b59e0f9e9b6
altsanz <asanz91@gmail.com>
2014-03-27 17:28:21 +0100

    ADD simple test to login controller

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -20,32 +20,3 @@ describe('Controller: MainCtrl', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
 });
-
-
-describe('Controller: LoginCtrl', function () {
-
-  // load the controller's module
-  beforeEach(module('angulApp'));
-
-  var LoginCtrl,
-    scope,
-    routeParams;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    
-    routeParams = {
-      socialNetwork : 'Google'
-    };
-
-    LoginCtrl = $controller('MainCtrl', {
-      $scope: scope,
-      $routeParams: routeParams
-    });
-  }));
-
-  it('should have the social network we use to login', function () {
-    expect(routeParams.socialNetwork).equals('Google');
-  });
-});
