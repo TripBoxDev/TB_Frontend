79477bf0451c9109182c3d15d9a3e35141df952c
altsanz <asanz91@gmail.com>
2014-03-28 17:36:45 +0100

    Boton de facebook hace peticion HTTP Get y actualiza un campo indicando si la peticion se ha hecho con éxito

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,53 @@ angular.module('angulApp')
       'Karma'
     ];
   })
-  .controller('LoginCtrl', function ($scope, $routeParams) {
-   	$scope.socialNetwork = $routeParams.socialNetwork;
+  .controller('LoginCtrl', function ($scope, $routeParams, $http, authService) {
+    $scope.socialNetwork = $routeParams.socialNetwork;
+
+    $scope.dato = authService.data;
+    //$scope.dato = authService.data;
+    $scope.login = function(){
+        // De pegote
+        var config = {
+          method : 'GET',
+          url: '/'
+        };
+
+        $http(config)
+          .success(function(data, status, headers, config) {
+            console.log(status === 200);
+            if (status === 200) {
+              // succefull login
+              authService.data.isLogged = true;
+              authService.data.username = data.username;
+              console.log(authService.data.isLogged);
+            } else {
+              authService.data.isLogged = false;
+              authService.data.username = '';
+              console.log(authService.data.isLogged);
+            }
+            $scope.dato = authService.data;
+
+          })
+          .error(function(data, status, headers, config) {
+            authService.data.isLogged = false;
+            authService.data.username = '';
+            console.log(authService.data.isLogged);
+            $scope.dato = authService.data;
+
+          });
+
+
+      }
+  })
+  .factory('authService', function() {
+    var authManagement = {
+      data: {
+        isLogged : false,
+        username : ''
+      }
+      
+    };
+    return authManagement;
+ 
   });
