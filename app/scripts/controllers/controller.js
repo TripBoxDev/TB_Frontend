4bafdfc64780a273e9186620650122bd31d962fa
CorreaBCN <correaroman@gmail.com>
2014-03-30 22:09:16 +0200

    Vista groups.html con angular encarada y funcionando

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -1,8 +1,11 @@
-function MainCtrl ($scope) {
+function getGroup ($scope) {
   $scope.groups = [
-    {id:1, name:'Miami'},
-    {id:2, name:'Berlin'},
-    {id:3, name:'Luxenburgo'}
+    {id:1, name:'Miami', description:'País muy bonito pq lo digo yo'},
+    {id:2, name:'Berlin', description:'Me encanta la cerveza negra'},
+    {id:3, name:'Luxenburgo', description:'Aquí se te ha ido de las manos Correa xd'},
+    {id:4, name:'Dublin', description:'Aquí se te ha ido de las manos Correa xd'},
+    {id:5, name:'Amsterdam', description:'Aquí se te ha ido de las manos Correa xd'},
+    {id:6, name:'Japon', description:'Aquí se te ha ido de las manos Correa xd'},
+    {id:7, name:'Hawai', description:'Aquí se te ha ido de las manos Correa xd'}
   ];
-
 }
