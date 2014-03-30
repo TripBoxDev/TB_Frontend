08633b04da677553f66c4e0df0ebde3513eb952d
CorreaBCN <correaroman@gmail.com>
2014-03-30 20:25:13 +0200

    Base de groups.html sin angularJS y intento fallido de angulasJS datos id y name group

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -1,11 +1,29 @@
-var app = angular.module('groupApp'), []);
+'use strict';
 
-	app.controller('controller_group', ['$scope', function($scope){
+
+
+function ControllerGroups ($scope) {
+
+  $scope.groups = [
+    {id:1, name:'Miami'},
+    {id:2, name:'Berlin'},
+    {id:3, name:'Luxenburgo'}
+  ];
+
+}
+
+
+
+
+
+
+angular.module('groupApp')
+
+	.controller('ControllerGroup', function ($scope) {
 
 		$scope.groups = [
 		    {id:1, name:'Miami'},
 		    {id:2, name:'Luxenburgo'},
 		    {id:3, name:'Berlin'}
 		];
-	}
-]);
+	});
