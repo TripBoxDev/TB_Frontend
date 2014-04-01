fe2249d61d1fca62133867e70b65dffe220918c6
altsanz <asanz91@gmail.com>
2014-04-01 16:14:03 +0200

    Añade tests para el controlador de login: - Asegura que al iniciar el controlador la variable que marca si esta logueado o no es falso - Asegura que cuando la conexión al llamar a login() se realiza con éxito la variable isLogged se actualiza

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -7,22 +7,53 @@ describe('Controller: LoginCtrl', function () {
 
   var LoginCtrl,
     scope,
-    routeParams;
+    routeParams,
+    authService,
+    httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
     scope = $rootScope.$new();
     routeParams = {
       socialNetwork : 'Google'
     };
+    authService = {
+      data: {
+        isLogged : false,
+        username : ''
+      }
+    };
+     
+
+    httpBackend = $httpBackend;
+    
+    httpBackend.when('GET', '/').respond({userId: 'userX'}, {'A-Token': 'xxx'});
 
     LoginCtrl = $controller('LoginCtrl', {
       $scope: scope,
-      $routeParams: routeParams
+      $routeParams: routeParams,
+      authService: authService
     });
   }));
 
-  it('should have the social network we use to login', function () {
+  it('should have the social network we used to login', function () {
     expect(scope.socialNetwork).toEqual('Google');
   });
+
+  it('should mark as not logged in when we haven\'t done a login', function() {
+    expect(authService.data.isLogged).toBeFalsy();
+  });
+
+
+  it('should mark as logged in when connection is successful', function() {
+    httpBackend.expectGET('/');
+    scope.login();
+
+    httpBackend.flush();
+    expect(authService.data.isLogged).toBeTruthy();
+
+  });
+
+
+
 });
