1fda67f9216c2fd4ec8f185959101ebaa8a71089
CorreaBCN <correaroman@gmail.com>
2014-04-02 11:29:39 +0200

    Añadido el GroupCtrl al main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,29 @@
 'use strict';
 
 angular.module('angulApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    })
+
+
+//PETICION JSON HACIA LA API
+
+//para hacer uso de $resource debemos colocarlo al crear el modulo
+
+//con dataResource inyectamos la factoría
+.controller("GroupsCtrl", function($scope) {
+
+    //hacemos uso de $http para obtener los datos del json
+    $http.get('data.json').success(function(data) {
+        //Convert data to array.
+        //datos lo tenemos disponible en la vista gracias a $scope
+        $scope.datos = data;
+    });
+    //datosResource lo tenemos disponible en la vista gracias a $scope
+    $scope.datosResource = dataResource.get();
+});
+
