2154939b13693a2ceff70e0769621c998e48436c
CorreaBCN <correaroman@gmail.com>
2014-04-02 11:44:56 +0200

    Añadido en el Main.js la variable groups y los metodos addGroup y unFollowGroup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,7 +16,25 @@ angular.module('angulApp')
 
 //con dataResource inyectamos la factoría
 .controller("GroupsCtrl", function($scope) {
+	$scope.groups = [
+	    {id:1, name:'Miami', description:'País muy bonito pq lo digo yo'},
+	    {id:2, name:'Berlin', description:'Me encanta la cerveza negra'},
+	    {id:3, name:'Luxenburgo', description:'Aquí se te ha ido de las manos Correa xd'},
+	    {id:4, name:'Dublin', description:'Aquí se te ha ido de las manos Correa xd'},
+	    {id:5, name:'Amsterdam', description:'Aquí se te ha ido de las manos Correa xd'},
+	    {id:6, name:'Japon', description:'Aquí se te ha ido de las manos Correa xd'},
+	    {id:7, name:'Hawai', description:'Aquí se te ha ido de las manos Correa xd'}
+	];
 
+	$scope.addGroup = function() {
+		$scope.groups.push({id:7, name:'Hawai', description:'Aquí se te ha ido de las manos Correa xd'});
+		console.log($scope.groups);
+	}
+
+	$scope.unfollowGroup = function(){
+		$scope.groups.pop();
+	}
+    /*
     //hacemos uso de $http para obtener los datos del json
     $http.get('data.json').success(function(data) {
         //Convert data to array.
@@ -25,5 +43,6 @@ angular.module('angulApp')
     });
     //datosResource lo tenemos disponible en la vista gracias a $scope
     $scope.datosResource = dataResource.get();
+    */
 });
 
