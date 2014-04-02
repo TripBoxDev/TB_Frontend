065594e8aa01e5753dfcbce70b72de8dce98cc60
CorreaBCN <correaroman@gmail.com>
2014-04-02 16:31:06 +0200

    Funcionalidad de eliminar grupos implementado en groups.html y el controlador en main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,24 +16,30 @@ angular.module('angulApp')
 
 //con dataResource inyectamos la factoría
 .controller("GroupsCtrl", function($scope) {
-	$scope.groups = [
-	    {id:1, name:'Miami', description:'País muy bonito pq lo digo yo'},
-	    {id:2, name:'Berlin', description:'Me encanta la cerveza negra'},
-	    {id:3, name:'Luxenburgo', description:'Aquí se te ha ido de las manos Correa xd'},
-	    {id:4, name:'Dublin', description:'Aquí se te ha ido de las manos Correa xd'},
-	    {id:5, name:'Amsterdam', description:'Aquí se te ha ido de las manos Correa xd'},
-	    {id:6, name:'Japon', description:'Aquí se te ha ido de las manos Correa xd'},
-	    {id:7, name:'Hawai', description:'Aquí se te ha ido de las manos Correa xd'}
-	];
+    $scope.groups = [
+        {id:1, name:'Miami', description:'País muy bonito pq lo digo yo'},
+        {id:2, name:'Berlin', description:'Me encanta la cerveza negra'},
+        {id:3, name:'Luxenburgo', description:'Aquí se te ha ido de las manos Correa xd'},
+        {id:4, name:'Dublin', description:'Aquí se te ha ido de las manos Correa xd'},
+        {id:5, name:'Amsterdam', description:'Aquí se te ha ido de las manos Correa xd'},
+        {id:6, name:'Japon', description:'Aquí se te ha ido de las manos Correa xd'},
+        {id:7, name:'Hawai', description:'Aquí se te ha ido de las manos Correa xd'}
+    ];
 
-	$scope.addGroup = function() {
-		$scope.groups.push({id:7, name:'Hawai', description:'Aquí se te ha ido de las manos Correa xd'});
-		console.log($scope.groups);
-	}
+    $scope.addGroup = function() {
+        $scope.groups.push({id:7, name:'Hawai', description:'Aquí se te ha ido de las manos Correa xd'});
+        console.log($scope.groups);
+    }
 
-	$scope.unfollowGroup = function(){
-		$scope.groups.pop();
-	}
+    $scope.unfollowGroup = function(id){
+        
+        for (var i = $scope.groups.length - 1; i >= 0; i--){
+            if ($scope.groups[i].id == id){
+                $scope.groups.splice(i,1);
+            }
+
+        }
+    }
     /*
     //hacemos uso de $http para obtener los datos del json
     $http.get('data.json').success(function(data) {
@@ -44,5 +50,7 @@ angular.module('angulApp')
     //datosResource lo tenemos disponible en la vista gracias a $scope
     $scope.datosResource = dataResource.get();
     */
-});
-
+})
+  .controller('LoginCtrl', function ($scope, $routeParams) {
+    $scope.socialNetwork = $routeParams.socialNetwork;
+  });
