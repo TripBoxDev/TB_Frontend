dfe3675dcc18c4881b8b98eca191c27e83f41f53
alonso8 <alonso_gonzalo_@hotmail.com>
2014-04-06 21:08:20 +0200

    Añadir y eliminar ok. No funciona el editar grupos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,81 +20,112 @@ angular.module('angulApp')
 
     //Intentamos leer los datos del usuario
     $http.get('http://tripbox.uab.cat/TB_Backend/api/user/' + user)
-        .success(function(result){
-            
+        .success(function(result) {
+
             //Si ha funcionado, recorre cada grupo de viaje al que pertenece el usuario
             for (var i = result["groups"].length - 1; i >= 0; i--) {
                 //Lee los datos del grupo
-                $http.get('http://tripbox.uab.cat/TB_Backend/api/group/'+ result["groups"][i])
-                .success(function(result){
+                $http.get('http://tripbox.uab.cat/TB_Backend/api/group/' + result["groups"][i])
+                    .success(function(result) {
 
-                    //Y si ha salido todo bien, añade a la lista del grupo los datos del grupo recién leido
-                    $scope.groups.push({id:result["id"], name:result["name"], description:result["description"]})
+                        //Y si ha salido todo bien, añade a la lista del grupo los datos del grupo recién leido
+                        $scope.groups.push({
+                            id: result["id"],
+                            name: result["name"],
+                            description: result["description"]
+                        })
 
-                });
+                    });
             };
         });
 
     $scope.addGroup = function(groupName, groupDescription) {
 
-        $scope.groups.push({id:'12345', name:groupName, description:groupDescription});
+        $scope.groups.push({
+            id: '778899',
+            name: groupName,
+            description: groupDescription
+        });
         console.log($scope.groups);
-        
+
         //Fututa funcion hacia la API
         /*
-        $http.put('http://tripbox.uab.cat/TB_Backend/api/group', {name:'New Group', description:'Prueba de Frontend0', users:123456})
+        $http.put('http://tripbox.uab.cat/TB_Backend/api/group', {name:groupName, description:groupDescription, users:'123456'})
         */
 
-    }
+    };
 
     $scope.editGroup = function(id, groupName, groupDescription) {
-        for (var i = $scope.groups.length - 1; i >= 0; i--){
-            if ($scope.groups[i].id == id){
+        for (var i = $scope.groups.length - 1; i >= 0; i--) {
+            if ($scope.groups[i].id == id) {
                 $scope.groups[i].name = groupName;
-                $scope.groups[i].description =  groupDescription;
+                $scope.groups[i].description = groupDescription;
 
                 //Función hacia la API
+                /*
+                $http.put('http://tripbox.uab.cat/TB_Backend/api/group', {name:groupName, description:groupDescription})
+                */
             }
         }
-    }
+    };
 
-    $scope.unfollowGroup = function(id){
-        
+    $scope.unfollowGroup = function(id) {
+
         console.log(id);
-        var user = "123456789"
-        var group = "987654321"
+        var user = "123456789";
+        var group = "987654321";
 
-        for (var i = $scope.groups.length - 1; i >= 0; i--){
-            if ($scope.groups[i].id == id){
-                $scope.groups.splice(i,1);
+        for (var i = $scope.groups.length - 1; i >= 0; i--) {
+            if ($scope.groups[i].id == id) {
+                $scope.groups.splice(i, 1);
 
                 //Fututa funcion hacia la API
                 /*
-                $http.delete('http://tripbox.uab.cat/TB_Backend/api/group/' + grpup + '/user/' + user)
+                $http.delete('http://tripbox.uab.cat/TB_Backend/api/group/' + group + '/user/' + user)
 
                 */
             }
 
         }
-    }
+    };
+
+    $scope.checkName = function(data) {
+        if (data !== '') {
+            return "Un grupo debe tener nombre";
+        }
+    };
+
+    $scope.saveGroup = function(data, id) {
+        //$scope.user not updated yet
+        angular.extend(data, {
+            id: id
+        });
+        //return $http.post('/saveUser', data);
+    };
+
+    // remove user
+    $scope.removeGroup = function(index) {
+        $scope.groups.splice(index, 1);
+    };
 
 })
 
 .directive('ngConfirmClick', [
-        function(){
-            return {
-                link: function (scope, element, attr) {
-                    var msg = attr.ngConfirmClick;
-                    var clickAction = attr.confirmedClick;
-                    element.bind('click',function (event) {
-                        if ( window.confirm(msg) ) {
-                            scope.$apply(clickAction)
-                        }
-                    });
-                }
-            };
-    }])
+    function() {
+        return {
+            link: function(scope, element, attr) {
+                var msg = attr.ngConfirmClick;
+                var clickAction = attr.confirmedClick;
+                element.bind('click', function(event) {
+                    if (window.confirm(msg)) {
+                        scope.$apply(clickAction)
+                    }
+                });
+            }
+        };
+    }
+])
 
-.controller('LoginCtrl', function ($scope, $routeParams) {
+.controller('LoginCtrl', function($scope, $routeParams) {
     $scope.socialNetwork = $routeParams.socialNetwork;
 });
