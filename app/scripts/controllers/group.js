ef73f2a3f8dd2848c6866dc425fc53675a33d498
altsanz <asanz91@gmail.com>
2014-04-23 13:04:26 +0200

    Incluye bootstrap.ui en bower y como modulo en la app

--- app/scripts/controllers/group.js
+++ app/scripts/controllers/group.js
@@ -1,4 +1,5 @@
-app.controller("GroupCtrl", function($scope, $routeParams, authService) {
+app.controller("GroupCtrl", function($scope, $routeParams, authService, $modal) {
 	$scope.groupId = $routeParams.groupId;
 	$scope.infoUser = authService.data.userInfo;
+
 });
