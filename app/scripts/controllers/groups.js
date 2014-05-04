f26014a5f378d5568792e511e1277e64ae4663d3
Raul-Ramos <amphiros@gmail.com>
2014-05-05 01:04:39 +0200

    Preparados los dos tipos de subida en vista y los dos tipos de subida en controlador

--- app/scripts/controllers/groups.js
+++ app/scripts/controllers/groups.js
@@ -49,6 +49,23 @@ app.controller("GroupsCtrl", function($scope, $http, authService, $modal) {
     //console.log(authService.data);
     $scope.infoUser = newUser; //authService.data.userInfo;
 
+    $scope.uploadFile = function(files) {
+        //METODO 1: Pasa la imagen directamente
+        $http.post("http://tripbox.uab.cat/TB_Backend2/api/group/zrUN09crbNSE/image", files[0]);
+
+        //METODO 2: Pasa encabezado
+        var fd = new FormData();
+        //Take the first selected file
+        fd.append("file", files[0]);
+        
+        //http://tripbox.uab.es/TB_Backend2/api/group/zrUN09crbNSE
+        /*$http.post("http://tripbox.uab.cat/TB_Backend2/api/group/zrUN09crbNSE/image", fd, {
+            withCredentials: true,
+            headers: {'Content-Type': undefined },
+            transformRequest: angular.identity
+        });*/
+
+    };
 
     $scope.editGroup = function(idGroup, groupName, groupDescription) {
 
