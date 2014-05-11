48e489870b2b66c7391657b6bf61461dd0f122f3
altsanz <asanz91@gmail.com>
2014-05-11 20:44:15 +0200

    Documenta receiveInvitation controller

--- app/scripts/controllers/receiveInvitation.js
+++ app/scripts/controllers/receiveInvitation.js
@@ -1,9 +1,14 @@
+/**
+ * Nombre:          ReceiveInvitationCtrl
+ * Descripcion:  	Controller encargado de recibir los usuarios que vengan de una invitación
+ * 					por email. Añade el usuario al grupo, y el grupo al usuario, después redirecciona
+ *					a la vista de grupos normal.
+ */
 app.controller("ReceiveInvitationCtrl", function($scope, $routeParams, ApiService, $log, $location) {
     $log.info('Inside receiveInvitationCtrl');
     if ($routeParams.invitation) {
         ApiService.addUserToGroup($routeParams.groupId).then(function() {
             $location.path('groups/' + $routeParams.groupId);
         });
-
     }
 });
