8b4e367c6f28b455fdbaf52bb6c0b0ecbb50d09d
santimunoz <santi.munozm@gmail.com>
2014-05-14 10:26:15 +0200

    Afegit service per guardar un group i poder-hi accedir des de qualsevol controlador

---
+++ app/scripts/services/groupService.js
@@ -0,0 +1,16 @@
+app.service('groupService', function(){
+
+	//afegir totes les variables necessaries
+	var group=null;
+
+	return {
+		getGroup:function(){
+			return group;
+		},
+		setGroup: function(newGroup){
+			group=newGroup;
+		}
+
+
+	};
+});
