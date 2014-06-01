90f2c502f01e6f3364468143077a1925b66fa9fc
CorreaBCN <correaroman@gmail.com>
2014-06-01 15:32:38 +0200

    Comienzo del controller

---
+++ app/scripts/controllers/check.js
@@ -0,0 +1,3 @@
+app.controller("CheckCtrl", function($rootScope, $scope, $routeParams, $location, authService, $modal, $http, ApiService, $log, notificationFactory, groupService, destiSelectedService, $q) {
+
+});
